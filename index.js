7b5296076284e9272c43fae8788b224929458603
taiwodevelops <taiwodevelops@gmail.com>
2025-01-08 10:22:42 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const fadeInElements = document.querySelectorAll('.fade-in');
+let a = ["Developer", "Gamer", "Farmer", "Child of God"];
+let ref = document.getElementById("text");
+let ind = 0, cInd = 0;
+let remove = false;
 
-    fadeInElements.forEach((element, index) => {
-        setTimeout(() => {
-            element.classList.add('visible');
-        }, index * 300); // Stagger the animations by 300ms
-    });
-});
+function typing() {
+    if (ind < a.length) {
+        let currentText = a[ind];
+        if (!remove && cInd < currentText.length) {
+            ref.textContent += currentText.charAt(cInd);
+            cInd++;
+            setTimeout(typing, 100);
+        } else if (remove && cInd >= 0) {
+            ref.textContent = currentText.substring(0, cInd);
+            cInd--;
+            setTimeout(typing, 100);
+        } else {
+            remove = !remove;
+            if (!remove) {
+                ind = (ind + 1) % a.length;
+            }
+            setTimeout(typing, 1000);
+        }
+    }
+}
+
+typing();
